4fc6fea5237927c47552c1e650bc788d2a78bcc3
Rocha <cl.rch09@icloud.com>
2024-06-10 15:22:34 -0300

    refactor: Update sendMessage function to use new API URL and improve message formatting

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -3,21 +3,21 @@ import { AppointmentDto } from './types'
 export const sendMessage = (appointment: AppointmentDto) => {
   const wppURL = import.meta.env.VITE_REACT_APP_API_URL
   const message = `
-  📅 MEU AGENDAMENTO%0D
-  👩 CLIENTE: *${appointment.name}*%0D
-  📱 TELEFONE: *${appointment.phone}*%0D
-  =-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
-  📌 DIA: *${appointment.appointmentDate}*%0D
-  🕛 HORÁRIO:  *${appointment.appointmentTime}*%0D
-  ✨ SERVIÇO: *${appointment.service}*%0D
-  💵 PREÇO: *${appointment.price}*%0D
-  =-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
-  👀 OBSERVAÇÕES%0D
-  *${appointment.observations ?? 'Sem observações'}*%0D
-  =-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
-  Obrigado pela preferencia!%0D
-  Espero que sua experiência seja incrível.%0D
-  =-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D 
-  COMPROVANTE DE AGENDAMENTO`
+📅 MEU AGENDAMENTO%0D
+👩 CLIENTE: *${appointment.name}*%0D
+📱 TELEFONE: *${appointment.phone}*%0D
+=-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
+📌 DIA: *${appointment.appointmentDate}*%0D
+🕛 HORÁRIO:  *${appointment.appointmentTime}*%0D
+✨ SERVIÇO: *${appointment.service}*%0D
+💵 PREÇO: *${appointment.price}*%0D
+=-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
+👀 OBSERVAÇÕES%0D
+*${appointment.observations === '' ? 'Sem observações' : appointment.observations}*%0D
+=-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D
+Obrigado pela preferencia!%0D
+Espero que sua experiência seja incrível.%0D
+=-=-=-=-=-=-=-=-=-=-=-=-=-==-=%0D 
+COMPROVANTE DE AGENDAMENTO`
   window.location.href = `${wppURL}${message}`
 }
